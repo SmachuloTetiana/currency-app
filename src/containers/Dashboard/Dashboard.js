11b8c2cb085baea7a07fcf25823264f342b46716
SmachuloTetiana <tanichk@ukr.net>
2020-02-23 23:20:31 +0200

    added Calculate Price Function

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -2,10 +2,28 @@ import React, { useState } from 'react';
 import Select from 'react-select';
 
 const Dashboard = () => {
-    const [selectedOption, setSelectedOption] = useState({
-        value: 'usd',
-        label: 'usd'
-    });
+    const tableValues = [
+        {
+            buyExchange: 'HITBTC/IOTA-USD',
+            buyPrice: 0.33063,
+            sellExchange: 'BINANCE/IOTA-USD',
+            sellPrice: 0.334,
+            tradeAmount: 353.3,
+            arbFree: '1.01%[0.15%]',
+            expPofit: 1,
+            expFees: 0.2
+        },
+        {
+            buyExchange: 'HITBTC/IOTA-USD',
+            buyPrice: 65.50671,
+            sellExchange: 'BINANCE/IOTA-USD',
+            sellPrice: 69.57002,
+            tradeAmount: 2.1449,
+            arbFree: '0.27%[0.15%]',
+            expPofit: 0.2,
+            expFees: 0.2
+        }
+    ];
 
     const options = [
         {
@@ -22,12 +40,29 @@ const Dashboard = () => {
         }
     ];
 
+    
+    const [selectedOption, setSelectedOption] = useState({
+        value: options[0].value,
+        label: options[0].label
+    });
+
     const handleChange = selectedOption => {
         setSelectedOption({
             value: selectedOption.value,
             label: selectedOption.label
         });
     }
+
+    const calculatePrice = price => {
+        switch(selectedOption.value) {
+            case 'eur':
+            return (price * 0.8).toFixed(2);
+            case 'uah':
+            return (price * 25).toFixed(2)
+            default:
+            return price;
+        }
+    };
 
     return (
         <section className="container-fluid">
@@ -38,22 +73,22 @@ const Dashboard = () => {
                             <a href="#">Clear filter</a>
 
                             <form className="d-flex flex-row">
-                                <div className="form-group mr-3">
+                                <div className="form-group mr-2">
                                     <label htmlFor="exchange">Buy advice exchange</label>
                                     <input className="form-control" type="text" name="exchange" />
                                 </div>
 
-                                <div className="form-group mr-3">
+                                <div className="form-group mr-2">
                                     <label htmlFor="currency">Buy advice currency</label>
                                     <input className="form-control" type="text" name="currency" />
                                 </div>
 
-                                <div className="form-group mr-3">
+                                <div className="form-group mr-2">
                                     <label htmlFor="currency">Buy advice currency</label>
                                     <input className="form-control" type="text" name="currency" />
                                 </div>
 
-                                <div className="form-group">
+                                <div className="form-group mr-2">
                                     <label htmlFor="sell_currency">Sell advice currency</label>
                                     <input className="form-control" type="text" name="sell_currency" />
                                 </div>
@@ -80,9 +115,20 @@ const Dashboard = () => {
                             </thead>
 
                             <tbody>
-                                <tr>
-
-                                </tr>
+                                {
+                                    tableValues.map((el, key) => (
+                                        <tr key={key}>
+                                            <td>{el.buyExchange}</td>
+                                            <td>{calculatePrice(el.buyPrice)}</td>
+                                            <td>{el.sellExchange}</td>
+                                            <td>{calculatePrice(el.sellPrice)}</td>
+                                            <td>{el.tradeAmount}</td>
+                                            <td>{el.arbFree}</td>
+                                            <td>{el.expPofit}</td>
+                                            <td>{el.expFees}</td>
+                                        </tr>
+                                    ))
+                                }
                             </tbody>
                         </table>
                     </div>
@@ -120,7 +166,7 @@ const Dashboard = () => {
 
                                     <div className="col text-center instances-container__block">
                                         <h4 className="instances-container__title">Active instance</h4>
-                                        <span>7</span>
+                                        <span>{tableValues.length}</span>
                                     </div>
 
                                     <div className="col text-center instances-container__block">
